3549f94710c243d2a2e4c836238425b3730ad3ca
h4hn <tobihahn@gmail.com>
2024-05-24 16:50:06 +0200

    fix: Tastaturlayout fix bei Signup und Passwort vergessen

--- lib/src/screens/reset_password.dart
+++ lib/src/screens/reset_password.dart
@@ -6,60 +6,58 @@ class ResetPassword extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
+      body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Expanded(
-              flex: 3,
-              child: Container(
-                alignment: Alignment.center,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image.asset('logo/flashr_logo.png', width: 200),
-                    SizedBox(height: 100),
-                    Text(
-                      'Passwort zurücksetzen',
-                      style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
-                    ),
-                  ],
-                ),
+            Container(
+              alignment: Alignment.center,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.12), // Dynamically adjust the height
+                  Image.asset(
+                    'assets/logo/flashr_logo.png',
+                    width: MediaQuery.of(context).size.height * 0.25,
+                  ),
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.06), // Dynamically adjust the height
+                  Text(
+                    'Passwort zurücksetzen',
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                ],
               ),
             ),
-            Expanded(
-              flex: 4,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    TextField(
-                      autofocus: true,
-                      decoration: InputDecoration(
-                        labelText: 'E-Mail',
-                      ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextField(
+                    autofocus: true,
+                    decoration: InputDecoration(
+                      labelText: 'E-Mail',
                     ),
-                    SizedBox(height: 20),
-                    ElevatedButton(
-                      onPressed: () {
-                        // Aktion für den Reset-Button hier einfügen
-                      },
-                      child: Text('Passwort zurücksetzen'),
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Aktion für den Reset-Button hier einfügen
+                    },
+                    child: Text('Passwort zurücksetzen'),
+                  ),
+                  SizedBox(height: 30),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pop(context); // Zurück zur vorherigen Seite
+                    },
+                    child: Text(
+                      'Zurück zum Login',
+                      style: TextStyle(fontSize: 14),
+                      textAlign: TextAlign.center,
                     ),
-                    SizedBox(height: 30),
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.pop(context); // Zurück zur vorherigen Seite
-                      },
-                      child: Text(
-                        'Zurück zum Login',
-                        style: TextStyle(fontSize: 14),
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
           ],
--- lib/src/screens/signup.dart
+++ lib/src/screens/signup.dart
@@ -55,76 +55,74 @@ class _SignupState extends State<Signup> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
+      body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Expanded(
-              flex: 3,
-              child: Container(
-                alignment: Alignment.center,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image.asset('logo/flashr_logo.png', width: 200),
-                    SizedBox(height: 100),
-                    Text(
-                      'Registrieren',
-                      style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
-                    ),
-                  ],
-                ),
+            Container(
+              alignment: Alignment.center,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.12), // Dynamically adjust the height
+                  Image.asset(
+                    'assets/logo/flashr_logo.png',
+                    width: MediaQuery.of(context).size.height * 0.25,
+                  ),
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.06), // Dynamically adjust the height
+                  Text(
+                    'Registrieren',
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                ],
               ),
             ),
-            Expanded(
-              flex: 4,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    TextField(
-                      controller: _usernameController,
-                      autofocus: true,
-                      decoration: InputDecoration(
-                        labelText: 'Username',
-                      ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextField(
+                    controller: _usernameController,
+                    autofocus: true,
+                    decoration: InputDecoration(
+                      labelText: 'Username',
                     ),
-                    SizedBox(height: 20),
-                    TextField(
-                      controller: _emailController,
-                      autofocus: true,
-                      decoration: InputDecoration(
-                        labelText: 'E-Mail',
-                      ),
+                  ),
+                  SizedBox(height: 20),
+                  TextField(
+                    controller: _emailController,
+                    autofocus: true,
+                    decoration: InputDecoration(
+                      labelText: 'E-Mail',
                     ),
-                    SizedBox(height: 20),
-                    TextField(
-                      controller: _passwordController,
-                      autofocus: true,
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        labelText: 'Passwort',
-                      ),
+                  ),
+                  SizedBox(height: 20),
+                  TextField(
+                    controller: _passwordController,
+                    autofocus: true,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: 'Passwort',
                     ),
-                    SizedBox(height: 20),
-                    ElevatedButton(
-                      onPressed: _signUp,
-                      child: Text('Sign Up'),
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: _signUp,
+                    child: Text('Sign Up'),
+                  ),
+                  SizedBox(height: 30),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pop(context); // Zurück zur vorherigen Seite
+                    },
+                    child: Text(
+                      'Zurück zum Login',
+                      style: TextStyle(fontSize: 14),
+                      textAlign: TextAlign.center,
                     ),
-                    SizedBox(height: 30),
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.pop(context); // Zurück zur vorherigen Seite
-                      },
-                      child: Text(
-                        'Zurück zum Login',
-                        style: TextStyle(fontSize: 14),
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
           ],
